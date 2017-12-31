a7fe5bf5185a6a93218c20c8ee632355556baaa1
asbsoft <ab2014box@gmail.com>
2017-12-31 15:45:13 +0200

    correct host-depended configurations

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -4,8 +4,8 @@ return [
         'db' => [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=yii2_uni2',
-            'username' => 'root',
-            'password' => 'root',
+            'username' => 'root', //
+            'password' => '???',  // todo: correct
             'charset' => 'utf8',
             'tablePrefix' => 'y2app_',
         ],
