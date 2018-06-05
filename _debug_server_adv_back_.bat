ded1026e56b94b2b3a8a5aaa7e0524c72fa29024
asbsoft <ab2014box@gmail.com>
2018-06-05 13:54:19 +0300

    fix PHP-server runners

--- _debug_server_adv_back_.bat
+++ _debug_server_adv_back_.bat
@@ -1,1 +1,1 @@
-php -S localhost:7777 -t %~dp0\backend\web .\_router_adv_back_.php
+php -S localhost:7777 -t %~dp0\backend\web %~dp0\_router_adv_back_.php
--- _debug_server_adv_front_.bat
+++ _debug_server_adv_front_.bat
@@ -1,1 +1,1 @@
-php -S localhost:9999 -t %~dp0\frontend\web .\_router_adv_front_.php
+php -S localhost:9999 -t %~dp0\frontend\web %~dp0\_router_adv_front_.php
